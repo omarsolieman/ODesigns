efcf3d027e8b2c10f097228bf09e3ecbd8aaf67e
Omar Medhat <omarmedhat2004@gmail.com>
2021-12-13 00:30:20 +0800

    added ganalytics

--- src/sections/featureSection.vue
+++ src/sections/featureSection.vue
@@ -1,14 +1,10 @@
 <template>
   <div class="relative bg-white pt-16 pb-32 overflow-hidden">
-  
     <feature-item-r class="mt-20">
     </feature-item-r>
     <feature-item-l class="mx-8">
     </feature-item-l>
-  
   </div>
-
-
 </template>
 
 <script>
