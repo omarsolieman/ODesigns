038747faf68ab3580af9a5cbe75745e704e8773f
Omar Medhat <omarmedhat2004@gmail.com>
2022-01-06 20:59:06 +0800

    removed vue meta package due to being unsupported

--- src/App.vue
+++ src/App.vue
@@ -19,28 +19,28 @@ export default {
     Navbar,
     Footer,
   },
-  data() {
-    return {
-      logo: require('./assets/preview.png')
-    }
-  },
-  metaInfo() {
-    return {
-      meta: [
-          // Twitter Card
-          {name: 'twitter:card', content: 'summary'},
-          {name: 'twitter:title', content: 'ODesigns'},
-          {name: 'twitter:description', content: 'A website design agency that does more than just design.'},
-          // image must be an absolute path
-          {name: 'twitter:image', content: this.logo},
-          // Facebook OpenGraph
-          {property: 'og:title', content: 'ODesigns'},
-          {property: 'og:site_name', content: 'ODesigns'},
-          {property: 'og:type', content: 'website'},
-          {property: 'og:image', content:  this.logo},
-          {property: 'og:description', content: 'A website design agency that does more than just design.'}
-      ]
-    }
-  }
+  // data() {
+  //   return {
+  //     logo: require('./assets/preview.png')
+  //   }
+  // },
+  // metaInfo() {
+  //   return {
+  //     meta: [
+  //         // Twitter Card
+  //         {name: 'twitter:card', content: 'summary'},
+  //         {name: 'twitter:title', content: 'ODesigns'},
+  //         {name: 'twitter:description', content: 'A website design agency that does more than just design.'},
+  //         // image must be an absolute path
+  //         {name: 'twitter:image', content: this.logo},
+  //         // Facebook OpenGraph
+  //         {property: 'og:title', content: 'ODesigns'},
+  //         {property: 'og:site_name', content: 'ODesigns'},
+  //         {property: 'og:type', content: 'website'},
+  //         {property: 'og:image', content:  this.logo},
+  //         {property: 'og:description', content: 'A website design agency that does more than just design.'}
+  //     ]
+  //   }
+  // }
 }
 </script>
