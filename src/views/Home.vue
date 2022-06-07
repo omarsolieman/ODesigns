2efe787b16415c8a9851d8b81e3938936c47a3b8
Omar Medhat <omarmedhat2004@gmail.com>
2022-06-07 19:50:30 +0800

    dss

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,8 +11,8 @@
             </div>
             <div class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
               <h1 class="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
-                <span class="block text-white">A Better way to</span>
-                <span class="block text-indigo-200">Market your business</span>
+                <span class="block text-white">Solutions For Your</span>
+                <span class="block text-indigo-200">Business</span>
               </h1>
               <p class="mt-6 max-w-lg mx-auto text-center text-xl text-indigo-200 sm:max-w-3xl">
                 With ODesigns you can have your own personalized website to market you or your business for the lowest prices.
